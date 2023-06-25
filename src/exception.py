2d94d0c5c9b0fe5e3e27f3be8947b2a5b9328e9c
MeghanaSai74818 <meghanareddy74818@gmail.com>
2023-06-25 16:55:59 -0500

    Updated exception handler

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,19 @@
 import sys
-import logging
-#  sys module allows you to interact with the Python runtime environment and perform various system-related tasks
+from src.logger import logging
 
 def error_message_details(error, error_details:sys):
     """"
     crafts an error message 
     """
 
-    _, _, exc_tb = error_details.exc_info() # gives the details of the exception like message, line number and so on 
-    file_name = exc_tb.tb_frame.f_code.co_filename # can find the structure in the custom exception handling documentation
+    _, _, exc_tb = error_details.exc_info()  
+    file_name = exc_tb.tb_frame.f_code.co_filename 
     error_message = "Error occurred in python script name[{0}] and at line number [{1}] with the error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
 
-# let us create a class so that we can call the above function when we get an error
+
 
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
@@ -26,7 +25,7 @@ class CustomException(Exception):
 
 
 
-# # to test
+# to test
 # if __name__=="__main__":
 #     try:
 #         result = 1/0
